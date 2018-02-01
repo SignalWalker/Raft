6567a6ef68b6749a7d3d08436f691e561f474ff1
Ash Walker <ashurstwalker@gmail.com>
2018-02-01 16:30:04 -0500

    bep

---
+++ Context.cs
@@ -0,0 +1,14 @@
+﻿namespace Raft {
+    using VulkanCore;
+    using VulkanCore.Khr;
+
+    public class Context {
+
+        public Context(Instance inst, SurfaceKhr surf) {
+
+
+
+        }
+
+    }
+}
---
+++ SurfaceSDL.cs
@@ -0,0 +1,10 @@
+﻿namespace Raft {
+    using System;
+    using VulkanCore;
+    using VulkanCore.Khr;
+    public class SurfaceSDL : SurfaceKhr {
+
+        protected internal SurfaceSDL(Instance parent, ref AllocationCallbacks? allocator, IntPtr handle) : base(parent, ref allocator, (long) handle) { }
+
+    }
+}
